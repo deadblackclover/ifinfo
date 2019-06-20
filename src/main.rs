b4b24dd2dd16ed78c22111d5b28524b68b410fbb
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-06-20 22:01:39 +0700

    Add files

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+extern crate interfaces2 as interfaces;
+
+use interfaces::Interface;
+use interfaces::Kind;
+use interfaces::Address;
+
+fn main() {
+    let ifaces = Interface::get_all().unwrap();
+    for iface in &ifaces {
+        let indent = " ".repeat(8);
+        print!("{}:", iface.name);
+        if iface.is_up() {
+            print!(" <UP>");
+        } else {
+            print!(" <DOWN>");
+        }
+
+        println!("  mtu {}", iface.get_mtu().unwrap());
+
+        for addr in &iface.addresses {
+            match addr.kind {
+                Kind::Packet => {},
+                Kind::Ipv4 => {
+                    print!("{}", indent);
+                    print!("inet ");
+                    pr(*addr);
+                },
+                Kind::Ipv6 => {
+                    print!("{}", indent);
+                    print!("inet6 ");
+                    pr(*addr);
+                },
+                Kind::Link => {},
+                _ => {},
+            }
+        }
+
+        print!("{}", indent);
+        println!("ether {}", iface.hardware_addr().unwrap().as_string());
+
+        println!(" ");
+    }
+}
+
+fn pr(addr: Address) {
+    if addr.addr.is_some() {
+        print!("{}", addr.addr.unwrap().ip());
+    }
+    if addr.mask.is_some() {
+        print!("  netmask {}", addr.mask.unwrap().ip());
+    }
+    if addr.hop.is_some() {
+        print!("  broadcast {}", addr.hop.unwrap());
+    }
+    println!("");
+}
